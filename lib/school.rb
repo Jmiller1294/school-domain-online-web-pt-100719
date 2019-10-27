7d31031dca9d390a1df065b3224941f2e901e297
Justin Miller <jayman1294@gmail.com>
2019-10-27 09:30:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,9 +11,13 @@ def initialize(name)
 end
 
 def add_student(name, grade)
+   @roster[grade] = [name]
   
   if @roster.include?(grade)
+    @roster[grade] << name
+  else
     
+  end
   
  
     
